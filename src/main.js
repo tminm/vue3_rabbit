34ecf22574528ada85144468ae58b4fca6008d4c
俊杰 陈 <2290157809@qq.com>
2023-05-12 20:31:46 +0800

    axios基础封装

--- src/main.js
+++ src/main.js
@@ -7,7 +7,10 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-
+import {getCategory} from '@/apis/testapi'
+getCategory().then(res =>{
+    console.log(res);
+})
 app.use(createPinia())
 app.use(router)
 

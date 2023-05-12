c2ab483be7143155f2420c9b82de8ca7a29a4466
俊杰 陈 <2290157809@qq.com>
2023-05-12 21:31:51 +0800

    项目整体路由配置

---
+++ src/views/Home/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h5>Home页</h5>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Layout/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>我是首页</h1>
+        <RouterView></RouterView>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>

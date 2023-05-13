1e090bb08873427093787f7b3dfeacd977633a53
俊杰 陈 <2290157809@qq.com>
2023-05-13 21:22:16 +0800

    product列表渲染

--- src/apis/home.js
+++ src/apis/home.js
@@ -11,3 +11,24 @@ export function getBannerAPI(){
 export const findNewAPI = () => { 
     return httpInstance({ url:'/home/new' 
  })}
+
+ //获取人气推荐
+ /**
+ * @description: 获取人气推荐
+ * @param {*}
+ * @return {*}
+ */
+ export const getHotAPI = () => {
+    return  httpInstance({url:'/home/hot'})
+  }
+
+  /**
+ * @description: 获取所有商品模块
+ * @param {*}
+ * @return {*}
+ */
+export const getGoodsAPI = () => {
+    return httpInstance({
+      url: '/home/goods'
+    })
+  }
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -15,11 +15,5 @@ import HomePanel from "./components/HomePanel.vue";
   <HomeNew />
   <HomeHot />
   <HomeProduct />
-  <!-- 测试面板组件 -->
-  <HomePanel title="新鲜好物" subtitle="新鲜好物 好多商品">
-    <div>新鲜好物插槽内容</div>
-  </HomePanel>
-  <HomePanel title="好物推荐" subtitle="人气推荐 好多商品">
-    <div>人气好物插槽内容</div>
-  </HomePanel>
+
 </template>

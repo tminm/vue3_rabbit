3b59f6e679ffdcce3cbe5c42c1e308ae8fade3b8
俊杰 陈 <2290157809@qq.com>
2023-05-13 20:17:49 +0800

    图片懒加载

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+import { useIntersectionObserver } from '@vueuse/core'
 const app = createApp(App)
 //测试接口
 // import {getCategory} from '@/apis/testapi'
@@ -16,3 +16,20 @@ app.use(createPinia())
 app.use(router)
 
 app.mount('#app')
+
+// 定义全局指令
+
+app.directive('img-lazy',{
+    mounted(el,binding){
+        console.log(el,binding);
+        useIntersectionObserver(
+            el,
+            ([{ isIntersecting }]) => {
+                //进入视口区域
+              if(isIntersecting){
+                el.src = binding.value
+              }
+            },
+          )
+    }
+})

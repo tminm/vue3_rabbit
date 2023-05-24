bf81fe31a983df483776aed2bfa922acf2b11df0
俊杰 陈 <2290157809@qq.com>
2023-05-24 19:23:52 +0800

    登录基础业务

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,6 +1,9 @@
 <script setup>
-
+import { ElMessage } from 'element-plus'
+import 'element-plus/theme-chalk/el-message.css'
 import { ref } from "vue";
+import {loginAPI} from '@/apis/user.js'
+import {useRouter} from 'vue-router'
 const form = ref({
   account:'',
   password:'',
@@ -33,15 +36,23 @@ const rules = {
 
 //获取表单form实例
 const formRef = ref(null)
+const router = useRouter()
 const doLogin = () =>{
+  const {account,password} = form.value
+
   //调用实例方法
-  formRef.value.validate((valid) =>{
+  formRef.value.validate(async (valid) =>{
     //valid :所以表单都通过校验 才为true
     console.log(valid);
     //以valid做为判断条件 如果通过校验才执行登录逻辑
     if(valid){
       //TODO LOGIN
-      
+      const res = await loginAPI({account,password})
+      // console.log(res);
+      //1、提示用户
+      ElMessage({type:'success',message:'登录成功'})
+      //2、跳转首页
+      router.replace({path:'/'})
     }
   })
 }

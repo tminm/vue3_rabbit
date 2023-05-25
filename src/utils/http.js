7531d76e716327c0394a886ae79a3e49bbfcb1df
俊杰 陈 <2290157809@qq.com>
2023-05-25 09:17:26 +0800

    登录-token的携带

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,5 @@
 // axios的基础封装
+import { useUserStore } from '@/stores/user'
 import axios from 'axios'
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
@@ -9,6 +10,13 @@ const httpInstance =  axios.create({
 
 // axios请求拦截器
   httpInstance.interceptors.request.use(config => {
+    //1、从pinia获取token数据
+    const userStore = useUserStore()
+    //2、按照后端的要求拼接token数据
+    const token = userStore.userInfo.token
+    if(token){
+      config.headers.Authorization = `Bearer ${token}`
+    }
     return config
   }, e => Promise.reject(e))
   

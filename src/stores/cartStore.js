d96d9513586a8fd3e20d97c958e707e67c05e646
俊杰 陈 <2290157809@qq.com>
2023-05-27 12:49:28 +0800

    本地购物车-计算逻辑

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -1,5 +1,5 @@
 import { defineStore } from "pinia";
-import {ref} from 'vue';
+import {computed, ref} from 'vue';
 
 export const useCartStore = defineStore('cart',() =>{
     //1、定义status
@@ -24,7 +24,14 @@ export const useCartStore = defineStore('cart',() =>{
         const idx = cartList.value.findIndex((item) => skuId === item.id)
         cartList.value.splice(idx,1)
     }
+    //计算属性
+    //1、总的数量 所有项的count之和
+    const allCount = computed(() => cartList.value.reduce((a,c) => a+c.count,0))
+    //2、总价 所有项的count*price之和
+    const allPrice = computed(() => cartList.value.reduce((a,c) => a + c.count * c.price,0))
     return{
+        allCount,
+        allPrice,
         cartList,
         addcart,
         delCart

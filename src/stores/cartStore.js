a103d9511c222bc12a63080a4f9ae42ab861e61b
俊杰 陈 <2290157809@qq.com>
2023-05-29 22:15:01 +0800

    合并购物车操作

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -84,7 +84,8 @@ export const useCartStore = defineStore('cart', () => {
         delCart,
         singleCheck,
         allCheck,
-        clearCart
+        clearCart,
+        updateNewList
     }
 }, {
     persist: true,

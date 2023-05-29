6b6c1aa1d688b6bc342b7750eba79b78bf6c0075
俊杰 陈 <2290157809@qq.com>
2023-05-29 22:48:07 +0800

    结算页-路由配置和数据跳转

--- src/router/index.js
+++ src/router/index.js
@@ -8,6 +8,7 @@ import Category from '@/views/Category/index.vue'
 import subCategory from '@/views/subCategory/index.vue'
 import Detail from '@/views/Detail/index.vue'
 import CartList from '@/views/CartList/index.vue'
+import Checkout from '@/views/Checkout/index.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //path和components放置的位置
@@ -31,9 +32,14 @@ const router = createRouter({
         {
           path:'/detail/:id',
           component:Detail
-        },{
+        },
+        {
           path:'/cartlist',
           component:CartList
+        },
+        {
+          path:'/checkout',
+          component:Checkout
         }
       ]
     },
